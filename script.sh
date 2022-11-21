4b7f3cdbe198d2c68e25ce211dfc54e195a1aebb
Albert Mink <albert.mink@sap.com>
2022-11-21 20:30:43 +0100

    another try

--- script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/SAP/abap-file-formats
-cp abap-file-formats/file-formats/zif_* ../abap-file-format-src/src/
-cp abap-file-formats/file-formats/*/type/zif_* ../abap-file-format-src/src/
-rm -rf abap-file-formats/
