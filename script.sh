c32a9c275e5e8afab4b4b4f8a7979c6f6d038996
Albert Mink <albert.mink@sap.com>
2022-11-21 20:44:06 +0100

    now let's work with the paths

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 git clone https://github.com/SAP/abap-file-formats
-cp abap-file-formats/file-formats/zif_* ../src/
-cp abap-file-formats/file-formats/*/type/zif_* ../src/
+cp abap-file-formats/file-formats/zif_* src/
+cp abap-file-formats/file-formats/*/type/zif_* src/
 rm -rf abap-file-formats/
