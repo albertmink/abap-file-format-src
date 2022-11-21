e8e8687eda5ef14a6b3330fb8ca133fd062da023
Albert Mink <albert.mink@sap.com>
2022-11-21 20:49:22 +0100

    add git status to script

--- script.sh
+++ script.sh
@@ -4,3 +4,4 @@ git clone https://github.com/SAP/abap-file-formats
 cp abap-file-formats/file-formats/zif_* src/
 cp abap-file-formats/file-formats/*/type/zif_* src/
 rm -rf abap-file-formats/
+git status
